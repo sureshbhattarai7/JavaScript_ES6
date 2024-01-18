d8acbe545b969ba7d5b176a7591ce3ab88def7a9
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-01-18 20:57:31 +0545

    Example of destructuring Object

--- destructuringObjects.js
+++ destructuringObjects.js
@@ -1,14 +1,26 @@
-const player = {
-    name: 'Cristiano Ronaldo',
-    club: 'Real Madrid',
-    address: {
-        city: 'Madrid'
+// const player = {
+//     name: 'Cristiano Ronaldo',
+//     club: 'Real Madrid',
+//     address: {
+//         city: 'Madrid'
+//     }
+// }
+
+// // console.log(player.name);
+
+// const { name, club, address: { city } } = player;
+// console.log(`${name} plays for ${club}`);
+
+// console.log(`${name} lives in ${city}`);
+
+
+const student = {
+    name: 'Suresh',
+    age: 23,
+    project: {
+        restAPI: 'Rest API using Node.js, express.js and MongoDB'
     }
 }
 
-// console.log(player.name);
-
-const { name, club, address: { city } } = player;
-console.log(`${name} plays for ${club}`);
-
-console.log(`${name} lives in ${city}`);
+const { name, age, project: { restAPI } } = student;
+console.log(`${name} of age ${age} has done personal project of ${restAPI}`);
